3028e249eb1e2e1851b7e349d6d187de6e913055
Ivan Pizarroso <ivnp31@gmail.com>
2022-08-17 19:46:14 +0200

    add projects

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const buttonMenu = document.querySelector(".menu-button");
+const linkMenu = document.querySelectorAll(".link-menu");
+const menu = document.querySelector(".show-menu");
+let view = false;
+
+linkMenu.forEach((item) =>
+  item.addEventListener("click", () => {
+    toShowMenu();
+  })
+);
+
+buttonMenu.addEventListener("click", () => {
+  toShowMenu();
+});
+
+const toShowMenu = () => {
+  view = !view;
+  if (view) {
+    menu.classList.add("translate");
+    buttonMenu.classList.add("active");
+  } else {
+    menu.classList.remove("translate");
+    buttonMenu.classList.remove("active");
+  }
+};
